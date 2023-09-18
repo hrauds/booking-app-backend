bc8b9b54620f05e6aba495e408a4c7cb1981027f
ignatj <ignatjasevits@gmail.com>
2023-09-18 18:35:25 +0300

    Add Spring Boot project skeleton

---
+++ src/main/java/com/bookservice/gametimebooking/GameTimeBookingApplication.java
@@ -0,0 +1,13 @@
+package com.bookservice.gametimebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTimeBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTimeBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookservice/gametimebooking/GameTimeBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookservice.gametimebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTimeBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
