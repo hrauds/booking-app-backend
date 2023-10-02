572833e4c06d87c5294222d510098d83d217baae
hrauds <hanna.raudsepp@gmail.com>
2023-10-02 12:29:35 +0300

    get customer by id endpoint

--- src/main/java/com/bookservice/gametimebooking/service/customer/CustomerService.java
+++ src/main/java/com/bookservice/gametimebooking/service/customer/CustomerService.java
@@ -3,21 +3,23 @@ package com.bookservice.gametimebooking.service.customer;
 import com.bookservice.gametimebooking.controller.customer.dto.CustomerDto;
 import com.bookservice.gametimebooking.model.Customer;
 import com.bookservice.gametimebooking.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
 
     private CustomerRepository customerRepository;
     private CustomerMapper customerMapper;
 
-    public CustomerService(CustomerRepository customerRepository, CustomerMapper customerMapper) {
-        this.customerRepository = customerRepository;
-        this.customerMapper = customerMapper;
-    }
-
     public void addCustomer(CustomerDto customer) {
         Customer newCustomer = customerMapper.toEntity(customer);
         customerRepository.save(newCustomer);
+    }
+
+    public CustomerDto getCustomer(Long id) {
+        var customer = customerRepository.getReferenceById(id);
+        return customerMapper.toCustomerDto(customer);
     }
 }
